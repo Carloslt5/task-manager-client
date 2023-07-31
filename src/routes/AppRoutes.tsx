28cde8f6b55055e7ca4c49a329c33f2e7b5e585b
Carloslt <carloslt.proyectos@gmail.com>
2023-07-31 21:58:21 +0200

    init

---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from 'react-router-dom'
+import HomePage from '../pages/HomePage/HomePage'
+import AboutPage from '../pages/AboutPage/AboutPage'
+
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={< HomePage />} />
+      < Route path="/about" element={< AboutPage />} />
+    </Routes>
+  )
+}
+
+export default AppRoutes
