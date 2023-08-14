bd88ca57dc9193cc2e63bfc4cac9a01e5dc5ef72
Carloslt <carloslt.proyectos@gmail.com>
2023-08-14 22:57:57 +0200

    add sidbar

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -2,79 +2,69 @@ import { useContext, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { AuthContext } from '../../contexts/auth.context'
 import { AuthContextType } from '../../contexts/Types/AuthContext.types'
-import HomeIcon from '../icons/HomeIcon'
-import LoginIcon from '../icons/LoginIcon'
 import ArrowRigthIcon from '../icons/ArrowRigthIcon'
 import ArrowLeftIcon from '../icons/ArrowLeftIcon'
+import Logo from '../icons/Logo'
+import { MdDashboard, MdOutlineListAlt } from 'react-icons/md'
 
 const Navigation = () => {
 
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  // const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
   const [toggleMenuOpen, setToggleMenuOpen] = useState(true)
   const { user, logout } = useContext(AuthContext) as AuthContextType
 
-  const mobileMenuHandler = () => {
-    setMobileMenuOpen(!mobileMenuOpen)
-  }
+  const MenuList = [
+    { title: 'Dashboard', src: '/profile', icon: MdDashboard },
+    { title: 'Task', src: '/', icon: MdOutlineListAlt },
+    { title: 'Inbox', src: 'Chat' },
+    { title: 'Accounts', src: 'User' },
+    { title: 'Schedule ', src: 'Calendar' },
+    { title: 'Search', src: 'Search' },
+    { title: 'Analytics', src: 'Chart' },
+    { title: 'Files ', src: 'Folder' },
+    { title: 'Setting', src: 'Setting' },
+  ]
+
+  // const mobileMenuHandler = () => {
+  //   setMobileMenuOpen(!mobileMenuOpen)
+  // }
 
   return (
-    <>
-      <nav className={`flex flex-col bg-gray-800 p-4 md:h-full relative duration-300 ${toggleMenuOpen ? 'md:w-60' : 'md:w-16'}`}>
+    <nav className={`bg-gray-900 h-screen flex flex-col p-4 pt-8 relative duration-300  ${toggleMenuOpen ? 'w-60' : 'w-16'}`}>
 
-        <div className='hidden md:block toggle-menu p-2 rounded absolute -right-4 bg-gray-800 border cursor-pointer z-10' onClick={() => setToggleMenuOpen(!toggleMenuOpen)}>
-          {!toggleMenuOpen ? <ArrowRigthIcon /> : <ArrowLeftIcon />}
-        </div>
+      <div className='toggleMenu p-2 rounded-full bg-gray-900 border border-white absolute top-3 -right-3' onClick={() => setToggleMenuOpen(!toggleMenuOpen)}>
+        {toggleMenuOpen ? <ArrowRigthIcon /> : <ArrowLeftIcon />}
+      </div>
 
-        <div className={`bg-green-800 flex justify-start md:flex-col md:gap-6 md:h-full ${toggleMenuOpen && 'items-stretch'} relative`}>
+      <Link to={'/'}>
+        <div className='navBarTop flex gap-x-4 items-center bg-gray-500 rounded'>
+          <span>
+            <Logo />
+          </span>
+          <h1 className={`text-1xl font-bold origin-left text-white whitespace-nowrap duration-300 ${!toggleMenuOpen && 'scale-0'}`}>TODO-APP</h1>
+        </div >
+      </Link>
 
-          <Link to='/' >
-            <div className='flex gap-4 items-center whitespace-nowrap p-2 w-fit'>
-              <HomeIcon />
-              <h1 className={`font-bold text-white ${toggleMenuOpen ? 'block' : 'hidden'}`}>TODO-APP</h1>
-            </div>
-          </Link>
-
-          <div className='flex items-end md:hidden'>
-            <button type='button' onClick={mobileMenuHandler} className='relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white' aria-controls='mobile-menu' aria-expanded='false'>
-              <svg className='block h-6 w-6' fill='none' viewBox='0 0 24 24' strokeWidth='1.5' stroke='currentColor' aria-hidden='true'>
-                <path strokeLinecap='round' d='M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5' />
-              </svg>
-              <svg className='hidden h-6 w-6' fill='none' viewBox='0 0 24 24' strokeWidth='1.5' stroke='currentColor' aria-hidden='true'>
-                <path strokeLinecap='round' d='M6 18L18 6M6 6l12 12' />
-              </svg>
-            </button>
-          </div>
-
-          <Link to='/' className='flex gap-2 items-center text-white w-fit hover:bg-gray-900 p-2  focus:bg-gray-900 rounded-md text-sm font-medium' aria-current='page'>
-            <HomeIcon />
-            <p className={`whitespace-nowrap bg-red-500  ${toggleMenuOpen ? 'block ' : 'block'}`}>HOME</p>
-          </Link>
-          {/* <Link to='/about' className='text-white hover:underline focus:underline rounded-md text-sm font-medium'>ABOUT</Link> */}
-          {!user
-            ? <Link to='/login' className='flex gap-2 items-center w-fit text-white hover:bg-gray-900 p-2  focus:bg-gray-900 rounded-md text-sm font-medium'>
-              <LoginIcon />
-              <p className={`whitespace-nowrap bg-red-500 ${toggleMenuOpen ? 'block ' : 'block'}`}>LOGIN / SIGNUP</p>
+      <div className='navBarItems'>
+        <ul className='pt-6'>
+          {MenuList.map((Menu, index) => (
+            <Link to={Menu.src} key={index}>
+              < li className={'flex rounded px-2 py-1 cursor-pointer hover:bg-light-white text-gray-300 tex t-sm items-center gap-x-4 hover:bg-gray-500 '} >
+                <span>
+                  {Menu.icon && <Menu.icon />}
+                </span>
+                <p className={`origin-left duration-300 ${!toggleMenuOpen && 'scale-0'}`}>
+                  {Menu.title}
+                </p>
+              </ li>
             </Link>
-            : <>
-              <Link to='/profile' className='text-white hover:underline focus:underline rounded-md text-sm font-medium'>PROFILE</Link>
-              <button onClick={logout} className='text-white hover:underline focus:underline rounded-md text-sm font-medium'>DISCONECT</button>
-            </>
-          }
 
-        </div>
+          ))}
+        </ul>
 
-        {/* <div className={mobileMenuOpen
-          ? 'flex flex-col justify-center absolute bg-gray-800 w-full left-0 origin-top-right  z-10'
-          : 'hidden md:hidden'}
-          id='mobile-menu'
-        >
-          <Link to='/' onClick={mobileMenuHandler} className='text-center text-white hover:underline focus:underline block rounded-md px-3 py-2 text-base font-medium' aria-current='page'>HOME</Link>
-          <Link to='/about' onClick={mobileMenuHandler} className='text-center text-gray-300 hover:underline focus:underline block rounded-md px-3 py-2 text-base font-medium'>ABOUT</Link>
-          <Link to='/profile' onClick={mobileMenuHandler} className='text-center text-gray-300 hover:underline focus:underline block rounded-md px-3 py-2 text-base font-medium'>PROFILE</Link>
-          <button onClick={logout} className='text-white hover:underline focus:underline rounded-md px-3 py-2 text-md font-medium'>DISCONECT</button>
-        </div> */}
-      </nav >
-    </>
+      </div>
+
+    </nav >
   )
 }
 
