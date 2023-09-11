2ade1e38326b8c235b28ce7c35da1ea464286101
Carloslt <carloslt.proyectos@gmail.com>
2023-09-11 17:58:05 +0200

    remove react dnd

--- src/components/ColumnState/ColumnState.tsx
+++ src/components/ColumnState/ColumnState.tsx
@@ -1,37 +1,22 @@
-import { useDrop } from 'react-dnd'
 import { IState } from '../../types/State.type'
 import AddNewTicket from '../AddNewTicket/AddNewTicket'
 import EachState from '../EachState/EachState'
 import EachTicket from '../EachTicket/EachTicket'
 import Loading from '../Loading/Loading'
-import { ITicketData } from '../../types/Ticket.type'
+
 import { useState } from 'react'
 
 const ColumnState: React.FC<IState> = (state) => {
   const [tickets, setTickets] = useState(state.ticket || [])
 
-  const [{ isOver }, drop] = useDrop(() => ({
-    accept: 'Ticket',
-    drop: (ticket: ITicketData) => addItemToSection(ticket),
-    collect: (monitor) => ({
-      isOver: !!monitor.isOver()
-    })
-  }))
-
-  const addItemToSection = (ticketToAdd: ITicketData) => {
-    console.log('droped', ticketToAdd, state.stateName)
-    setTickets((prevTickets) => [...prevTickets, ticketToAdd])
-  }
-
   return (
     <li>
       <article
-        ref={drop}
         className='flex flex-col gap-2 p-2 border min-w-[15rem] bg-slate-800 rounded max-h-[100%]'
       >
         <EachState {...state} />
 
-        <article className={`py-2 overflow-y-scroll rounded ${isOver && 'bg-slate-950'}`}>
+        <article className='py-2 overflow-y-scroll rounded bg-slate-950'>
           <ul
             className='flex flex-col gap-2 overflow-y-hidden'>
             {!tickets
--- src/components/EachTicket/EachTicket.tsx
+++ src/components/EachTicket/EachTicket.tsx
@@ -1,26 +1,10 @@
-import { useDrag } from 'react-dnd'
 import { ITicketData } from '../../types/Ticket.type'
 
-const EachTicket: React.FC<ITicketData> = ({ _id, title, completed, projectId, owner }) => {
-
-  const [{ isDragging }, drag] = useDrag(() => ({
-    type: 'Ticket',
-    item: {
-      _id: _id,
-      title: title,
-      completed: completed,
-      projectId: projectId,
-      owner: owner
-    },
-    collect: (monitor) => ({
-      isDragging: !!monitor.isDragging()
-    })
-  }))
+const EachTicket: React.FC<ITicketData> = ({ _id, title }) => {
 
   return (
     <li
-      ref={drag}
-      className={`p-1 bg-gray-700 rounded cursor-pointer hover:bg-gray-900 ${isDragging && 'opacity-30'}`}
+      className='p-1 bg-gray-700 rounded cursor-pointer hover:bg-gray-900 opacity-30'
     >
       <p>{title}</p>
       <p className='text-sm'>este es el id: </p>
