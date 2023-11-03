fe49476131dde547102bf1efd338e6a442029646
Carloslt <carloslt.proyectos@gmail.com>
2023-11-03 10:53:35 +0100

    add todo custom hook

--- src/components/AddNewTodo/AddNewTodo.tsx
+++ src/components/AddNewTodo/AddNewTodo.tsx
@@ -1,43 +1,14 @@
-import { useContext, useState } from 'react'
-import todoservices from '@/services/ToDo.services'
-import { useParams } from 'react-router-dom'
-import { ToDoContext } from '@/contexts/todo.context'
-import { ToDoContextType } from '@/contexts/Types/ToDoContext.types'
 import { ITicketData } from '@/types/Ticket.type'
-import { useForm } from 'react-hook-form'
-import { ValidationError } from '../SignupForm/SignupForm'
-import { AxiosError } from 'axios'
-import { TodoData } from '@/types/Todo.type'
-import { toast } from 'react-toastify'
+import { useAddTodo } from './useAddTodo-Hook'
 
 const AddNewTodo: React.FC<ITicketData> = ({ _id: ticketID }) => {
-  const { id: userID } = useParams()
-  const { addTodo } = useContext(ToDoContext) as ToDoContextType
 
-  const todoForm = useForm<TodoData>({
-    defaultValues: {
-      title: ''
-    }
-  })
-
-  const { register, handleSubmit } = todoForm
-  const [newTicketErrors, setNewTicketrrors] = useState<ValidationError[]>([])
-
-  const todoSubmithandler = async (newTodo: TodoData) => {
-    try {
-      const { data } = await todoservices.createToDo(userID!, newTodo, ticketID!)
-      addTodo(userID!, data, ticketID!)
-      setNewTicketrrors([])
-      todoForm.setValue('title', '')
-    } catch (error) {
-      if (error instanceof AxiosError) {
-        if (error.response?.status === 404 || error.response?.status === 422) {
-          toast.error(error.response?.data.message)
-        }
-        setNewTicketrrors(error.response?.data)
-      }
-    }
-  }
+  const {
+    register,
+    handleSubmit,
+    newTicketErrors,
+    todoSubmithandler
+  } = useAddTodo(ticketID)
 
   return (
     <>
