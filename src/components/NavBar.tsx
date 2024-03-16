99e4030de9e6b40d9399c7dd09aa9cc94d23ae52
Carloslt <carloslt.proyectos@gmail.com>
2024-03-16 17:50:15 +0100

    ✅ task: add icons and navbar hook

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,38 +1,40 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { APP_NAME } from '../constants/Menu.const';
+import { useNavBar } from '../hooks/useNavBar';
 import { Logo } from './Logo';
 import { ArrowLeft } from './icons/ArrowLeft';
 import { ArrowRight } from './icons/ArrowRight';
 import { Dark } from './icons/Dark';
+import { Dashboard } from './icons/Dashboard';
+import { Home } from './icons/Home';
 import { Light } from './icons/Light';
+import { Login } from './icons/Login';
 
-const initialThemeMode = localStorage.getItem('theme') === 'dark';
+const MENU_CONST_USER = [
+  {
+    title: 'Home',
+    src: '/',
+    icon: <Home />,
+  },
+  {
+    title: 'Dashboard',
+    src: `/dashboard`,
+    icon: <Dashboard />,
+  },
+  {
+    title: 'Login',
+    src: `/login`,
+    icon: <Login />,
+  },
+];
 
 const Navigation = () => {
-  const [darkMode, setDarkMode] = useState(initialThemeMode);
-  const [toggleMenuOpen, setToggleMenuOpen] = useState(false);
+  const { darkMode, setThemeMode, toggleMenu, toggleMenuOpen, toggleThemeHandler } = useNavBar();
 
   useEffect(() => {
     setThemeMode(darkMode);
-  }, [darkMode]);
-
-  const setThemeMode = (darkMode: unknown) => {
-    if (darkMode) {
-      document.documentElement.classList.add('dark');
-      localStorage.setItem('theme', 'dark');
-    } else {
-      document.documentElement.classList.remove('dark');
-      localStorage.setItem('theme', 'light');
-    }
-  };
-
-  const toggleMenu = () => {
-    setToggleMenuOpen(!toggleMenuOpen);
-  };
-  const toggleThemeHandler = () => {
-    setDarkMode(!darkMode);
-  };
+  }, [darkMode, setThemeMode]);
 
   return (
     <nav
@@ -60,6 +62,26 @@ const Navigation = () => {
           </h1>
         </div>
       </Link>
+      <aside className="mt-10">
+        {MENU_CONST_USER.map((menuItem, idx) => (
+          <li
+            className={
+              'rounded cursor-pointer list-none text-blue-chill-50	p-1 hover:bg-blue-chill-600 hover:dark:bg-blue-chill-950 transition ease-in-out'
+            }
+            key={idx}
+            title={menuItem.title}
+          >
+            <Link to={menuItem.src} className="flex items-center gap-4 ">
+              <span className="p-1">{menuItem.icon}</span>
+              <p
+                className={`text-md origin-left whitespace-nowrap duration-300 text-white ${!toggleMenuOpen && 'scale-0'}`}
+              >
+                {menuItem.title}
+              </p>
+            </Link>
+          </li>
+        ))}
+      </aside>
 
       <span
         className={`w-8 p-2 flex ${toggleMenuOpen ? 'justify-start' : 'justify-center'}  mt-auto text-white rounded cursor-pointer`}
