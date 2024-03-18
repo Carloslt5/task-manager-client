5741d77e611388f2bcb4d9b6b2f104c60b4a1134
Carloslt <carloslt.proyectos@gmail.com>
2024-03-18 17:08:13 +0100

    ✅ feature: install tanstack and get project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import { AppRoutes } from '@/routes';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 60,
+    },
+  },
+});
 
 function App() {
   return (
     <>
-      <AppRoutes />
+      <QueryClientProvider client={queryClient}>
+        <AppRoutes />
+      </QueryClientProvider>
     </>
   );
 }
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,24 +1,34 @@
 import { ProjectCard } from '@/features/project/components/ProjectCard';
 import projectservices from '@/services/project.services';
 import { useAuthStore } from '@/store/authStore';
-import { useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
 import { Link } from 'react-router-dom';
+// import { useEffect, useState } from 'react';
 
 export const DashboardPage = () => {
   const user = useAuthStore((state) => state.user);
-  const [projectData, setProjectData] = useState<Project[]>([]);
+  // const [projectData, setProjectData] = useState<Project[]>([]);
 
-  useEffect(() => {
-    loadProject();
-  }, []);
+  const query = useQuery({
+    queryKey: ['project'],
+    queryFn: () => {
+      return projectservices.getProject();
+    },
+  });
+  const projectData = query.data?.data || []; // Si query.data no está definido, asigna un array vacío
+  console.log('🚀 --------- query', projectData);
 
-  const loadProject = async () => {
-    const { data } = await projectservices.getProject();
-    setProjectData(data.data);
-  };
+  // useEffect(() => {
+  //   loadProject();
+  // }, []);
 
-  if (!projectData) return <h1>Loading...</h1>;
+  // const loadProject = async () => {
+  //   const { data } = await projectservices.getProject();
+  //   setProjectData(data.data);
+  // };
 
+  if (query.isLoading) return <h1>Loading...</h1>;
+  if (query.isError) return <h1>Error: {query.error.message}</h1>;
   return (
     <>
       <p>Esto es el store USERID, {user?.id}</p>
@@ -28,12 +38,11 @@ export const DashboardPage = () => {
       </header>
 
       <section className="grid w-full gap-2 mb-4 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 max-h-[500px] overflow-y-auto">
-        {projectData?.length > 0 &&
-          projectData.map((project) => (
-            <Link to={`/project/${project.id}`} key={project.id}>
-              <ProjectCard {...project} />
-            </Link>
-          ))}
+        {projectData.data.map((project: Project) => (
+          <Link to={`/project/${project.id}`} key={project.id}>
+            <ProjectCard {...project} />
+          </Link>
+        ))}
       </section>
     </>
   );
