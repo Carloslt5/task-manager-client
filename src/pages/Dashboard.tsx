58398154d55c4cacc2ee3998cb67ac1066eb2607
Carloslt <carloslt.proyectos@gmail.com>
2024-03-18 21:01:35 +0100

    ✨ task: add skeleton project

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,12 +1,9 @@
 import { ProjectCard } from '@/features/project/components/ProjectCard';
 import projectservices from '@/services/project.services';
-import { useAuthStore } from '@/store/authStore';
 import { useQuery } from '@tanstack/react-query';
 import { Link } from 'react-router-dom';
 
 export const DashboardPage = () => {
-  const user = useAuthStore((state) => state.user);
-
   const query = useQuery({
     queryKey: ['project'],
     queryFn: () => {
@@ -14,16 +11,13 @@ export const DashboardPage = () => {
     },
   });
 
-  if (query.isLoading) return <h1>Loading...</h1>;
+  if (query.isLoading) return <ProjectSkeleton />;
   if (query.isError) return <h1>Error: {query.error.message}</h1>;
   return (
     <>
-      <p>Esto es el store USERID, {user?.id}</p>
-
       <header>
         <h1 className="mb-3 title__primary">My Dashboard</h1>
       </header>
-
       <section className="grid w-full gap-2 mb-4 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 max-h-[500px] overflow-y-auto">
         {query &&
           query.data?.map((project) => (
@@ -35,3 +29,17 @@ export const DashboardPage = () => {
     </>
   );
 };
+
+export const ProjectSkeleton = () => (
+  <>
+    <header className="animate-pulse">
+      <div className="w-full h-12 mb-3 bg-gray-200 rounded"></div>
+    </header>
+    <section className="grid w-full gap-2 mb-4 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 max-h-[500px] overflow-y-auto animate-pulse">
+      <div className="h-20 bg-gray-200 rounded"></div>
+      <div className="h-20 bg-gray-200 rounded"></div>
+      <div className="h-20 bg-gray-200 rounded"></div>
+      <div className="h-20 bg-gray-200 rounded"></div>
+    </section>
+  </>
+);
