b98c7a9aaafbbfded8d97103e3f0c8cde512862d
Carloslt <carloslt.proyectos@gmail.com>
2024-06-22 00:51:07 +0200

    📦 build: add react query and init config

---
+++ src/app/AppProviders.tsx
@@ -0,0 +1,12 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import { queryClient as defaultQueryClient } from "./queryClient";
+
+type Props = {
+  readonly children: React.ReactNode;
+  readonly queryClient?: QueryClient;
+};
+
+export function AppProviders({ children, queryClient = defaultQueryClient }: Props) {
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+}
