0195d0f5818c859d08e9e72e6d7d5bddb95da9b9
Carloslt <carloslt.proyectos@gmail.com>
2024-06-23 18:01:42 +0200

    ✨ style: update project card style

--- src/domains/projects/components/ProjectCard.tsx
+++ src/domains/projects/components/ProjectCard.tsx
@@ -3,7 +3,8 @@ import { Project } from "../projects.type";
 export const ProjectCard = ({ title, description }: Project) => {
   return (
     <article className="card__primary">
-      <h2>{title}</h2>
+      <h2 className="text-xl">{title}</h2>
+      <hr className="my-3 bg-blue-chill-300 border-blue-chill-200" />
       <p className="text-sm">{description}</p>
     </article>
   );
