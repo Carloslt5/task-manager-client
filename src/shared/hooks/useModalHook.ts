94614430b90dd68f85ab2a83f6422f9e993c30c9
Carloslt <carloslt.proyectos@gmail.com>
2024-06-23 12:20:18 +0200

    ✅ feat: add modal and project form modal

---
+++ src/shared/hooks/useModalHook.ts
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+export const useModalHook = () => {
+  const [showModal, setShowModal] = useState(false);
+  const toggleModal = () => setShowModal(!showModal);
+
+  return {
+    showModal,
+    toggleModal,
+  };
+};
